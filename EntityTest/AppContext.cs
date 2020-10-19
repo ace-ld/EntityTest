b6a2e50cd81d95e78ff39097e114e9d0419803bb
ace-ld <54129812+ace-ld@users.noreply.github.com>
2020-10-19 20:44:10 +0300

    Add project files.

---
+++ EntityTest/AppContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityTest
+{
+    class AppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ EntityTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace EntityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using AppContext db = new AppContext();
+            User user1 = new User { Name = "Tom", Age = 33 };
+            User user2 = new User { Name = "Ann", Age = 35 };
+
+            db.Users.Add(user1);
+            db.Users.Add(user2);
+            db.SaveChanges();
+
+            var users = db.Users.ToList();
+
+            foreach (var u in users)
+                Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+        }
+    }
+}
